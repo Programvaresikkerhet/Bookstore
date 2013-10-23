c63619f07d9def544cc34cd890135e9b37fe0841
Thea Christine Mathisen <tcmathisen@gmail.com>
2013-10-23 17:48:56 +0200

    Made functions static

--- src/amu/model/Validation.java
+++ src/amu/model/Validation.java
@@ -5,33 +5,20 @@ import javax.mail.internet.InternetAddress;
 
 public class Validation {
 	
-	private String issues = "";
-	
 	private static int PASSWORD_LENGTH = 8;
 	
-	public boolean validatePassword(String password){
+	public static boolean validatePassword(String password){
 		boolean isValid = true;
 		if(password == "" || password == null || password.isEmpty()){
 			isValid = false;
 		} else{
-			if((password.length() < PASSWORD_LENGTH)){
-				issues += "The password must be at least " + PASSWORD_LENGTH + " characters long.";
-				isValid = false;
-			}
-			if(!hasUpperCase(password)){
-				issues += "\nThe password must have at least one uppercase letter.";
-				isValid = false;
-			}
-			if(!hasDigit(password)){
-				issues += "\nThe password must have at least one digit.";
-				isValid = false;
-			}
+			isValid = (hasCorrectLength(password) && hasUpperCase(password) && hasDigit(password));
 		}
 		return isValid;
 	}
 	
 	
-	public boolean validateEmail(String email){
+	public static boolean validateEmail(String email){
 		boolean isValid = true;
 		try{
 			InternetAddress emailAddress = new InternetAddress(email);
@@ -43,16 +30,11 @@ public class Validation {
 		
 	}
 	
-	
-	public String getIssues(){
-		return this.issues;
-	}
-	
-	public boolean hasCorrectLength(String password){
+	public static boolean hasCorrectLength(String password){
 		return (password.length() >= PASSWORD_LENGTH);
 	}
 	
-	public boolean hasUpperCase(String password){
+	public static boolean hasUpperCase(String password){
 		for(int i = 0; i < password.length(); i++){
 			if(Character.isUpperCase(password.charAt(i))){
 				return true;
@@ -61,7 +43,7 @@ public class Validation {
 		return false;
 	}
 	
-	public boolean hasDigit(String password){
+	public static boolean hasDigit(String password){
 		for(int i = 0; i < password.length(); i++){
 			if(Character.isDigit(password.charAt(i))){
 				return true;
